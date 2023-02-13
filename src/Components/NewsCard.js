3fc695db688a649482c99b30f2664b014aac2827
Anis Shreyan <anisasru@gmail.com>
2023-02-13 19:07:30 +0530

    news app

---
+++ src/Components/NewsCard.js
@@ -0,0 +1,14 @@
+import React from 'react'
+
+function NewsCard() {
+  return (
+    <>
+    <div className="news-card">
+        <img src="https://resize.indiatvnews.com/en/resize/newbucket/715_-/2020/11/breaking-1603159815-1606179737.jpg" />
+        <h3>Lorem ipsum, dolor sit amet consectetur adipisicing.</h3>
+    </div>
+    </>
+  )
+}
+
+export default NewsCard
---
+++ src/Components/NewsGrid.js
@@ -0,0 +1,25 @@
+import React from "react";
+import NewsCard from "./NewsCard";
+
+function NewsGrid() {
+  return (
+    <>
+      <div className="news-grid">
+        <NewsCard />
+        <NewsCard />
+        <NewsCard />
+        <NewsCard />
+        <NewsCard />
+        <NewsCard />
+        <NewsCard />
+        <NewsCard />
+        <NewsCard />
+        <NewsCard />
+        <NewsCard />
+        <NewsCard />
+      </div>
+    </>
+  );
+}
+
+export default NewsGrid;
