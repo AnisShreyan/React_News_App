bc5904fd69f217a28f1284bb55df2620efcf2bc6
Anis Shreyan <anisasru@gmail.com>
2023-02-13 19:36:26 +0530

    news app

--- src/Components/NewsCard.js
+++ src/Components/NewsCard.js
@@ -1,14 +1,14 @@
-import React from 'react'
+import React from "react";
 
-function NewsCard() {
+function NewsCard({ img, title }) {
   return (
     <>
-    <div className="news-card">
-        <img src="https://resize.indiatvnews.com/en/resize/newbucket/715_-/2020/11/breaking-1603159815-1606179737.jpg" />
-        <h3>Lorem ipsum, dolor sit amet consectetur adipisicing.</h3>
-    </div>
+      <div className="news-card">
+        <img src={img} />
+        <h3>{title}</h3>
+      </div>
     </>
-  )
+  );
 }
 
-export default NewsCard
+export default NewsCard;
--- src/Components/NewsGrid.js
+++ src/Components/NewsGrid.js
@@ -1,22 +1,29 @@
-import React from "react";
+import axios from "axios";
+import React, { useEffect, useState } from "react";
 import NewsCard from "./NewsCard";
 
 function NewsGrid() {
+    const [news, setNews] = useState([]);
+
+    useEffect(() => {
+      axios
+        .get(
+          "https://newsapi.org/v2/everything?q=apple&from=2023-02-12&to=2023-02-12&sortBy=popularity&apiKey=bd0e8817e8d24bac8a1430910931bbd4"
+        )
+        .then((data) => {
+          setNews([...data.data.articles]);
+        });
+    }, []);
+
+    console.log(news);
+
+
   return (
     <>
       <div className="news-grid">
-        <NewsCard />
-        <NewsCard />
-        <NewsCard />
-        <NewsCard />
-        <NewsCard />
-        <NewsCard />
-        <NewsCard />
-        <NewsCard />
-        <NewsCard />
-        <NewsCard />
-        <NewsCard />
-        <NewsCard />
+        {news.map((x, y) => (
+          <NewsCard img={x.urlToImage} title={x.title} key={y} />
+        ))}
       </div>
     </>
   );
